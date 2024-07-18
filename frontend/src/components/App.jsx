3738fd1df1467f4126f3e9b4b72b3486ff030484
SahilTyagii <tyagi28sahil@gmail.com>
2024-07-18 12:01:27 +0530

    react vite setup

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Logo from './Logo'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-auto flex flex-wrap justify-center'>
+        <Logo />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import './Logo.css'
+
+const Logo = () => {
+  return (
+    <div>
+      <h1 className="logo">QW<span className='I'>I</span>Z</h1>
+    </div>
+  )
+}
+
+export default Logo
