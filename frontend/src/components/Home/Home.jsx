ab59036c87911a368a9166ce5e7024540355795e
SahilTyagii <tyagi28sahil@gmail.com>
2024-07-19 18:25:19 +0530

    fixed responsiveness for mobile devices

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -4,7 +4,6 @@ import Logo from '../Logo/Logo.jsx'
 import Fox from '../../assets/fox.png'
 import Porc from '../../assets/porc.png'
 import Waves from '../../assets/blue_wave.png'
-import Pattern from '../Pattern.jsx'
 
 
 function Home() {
@@ -17,16 +16,15 @@ function Home() {
         Quiz, Compete, Conquer!
         <img className='size-8 m-1' src="https://em-content.zobj.net/source/apple/391/nerd-face_1f913.png"/></p>
       </div>
-      {/* <div className='w-full flex flex-row justify-center absolute'>
+      <div className='w-full flex flex-row justify-center absolute'>
         <img className='w-full' src={Waves} alt="waves" />
-      </div> */}
-      <Pattern />
-      <div className='flex md:flex-row flex-col md:justify-around flex-wrap justify-center'>
+      </div>
+      <div className='flex md:flex-row flex-col md:justify-around flex-wrap justify-center' style={{backgroundImage: "url(https://www.transparenttextures.com/patterns/arches.png)", backgroundColor:"white"}}>
       <img className='md:size-96 size-60 mx-auto hover:scale-110 ease-in-out duration-300 z-10' src={Fox} alt="fox" />
 
-      <div className='flex flex-col'>
+      <div className='flex flex-col w-full'>
         <div className='m-1'>
-        <button className='bg-red-700 rounded-full px-36 py-3 text-3xl my-red-btn hover:bg-red-900 my-10 z-10'>Singleplayer</button>
+        <button className='bg-red-700 rounded-full px-32 md:px-36 py-3 text-3xl my-red-btn hover:bg-red-900 my-10 z-10'>Singleplayer</button>
       </div>
       <div className='m-1'>
         <button className='bg-black rounded-full px-36 py-3 text-3xl text-white my-black-btn z-10'>Multiplayer</button>
