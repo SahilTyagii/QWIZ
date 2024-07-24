3aa3c144cd3bed80e6d276b254d88b1bf401a48d
SahilTyagii <tyagi28sahil@gmail.com>
2024-07-24 20:26:47 +0530

    changed routes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:4000"
+      "/api": "https://qwiz.up.railway.app/"
     },
   },
   plugins: [react()],
